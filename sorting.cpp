4fd621fd56dee7ed2286565fa44ca5db05c2e8ca
Kshitiz01 <kshitizv289@gmail.com>
2026-08-16 21:35:18 +0530

    bubble sort

--- sorting.cpp
+++ sorting.cpp
@@ -14,6 +14,19 @@ void selection_sort(int arr[],int n){
         
     }
 }
+
+// bubble sort
+void bubble_sort(int arr[],int n){
+    for(int i = n-1;i<=0;i--){
+        for(int j = 0;j<=i-1;j++){
+            if(arr[j] > arr[j+1]){
+                int temp = arr[j+1];
+                arr[j+1] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+}
 int main(){
 
     int n;
@@ -23,6 +36,12 @@ int main(){
         cin >> arr[i];
     }
         selection_sort(arr,n);
+        for(int i = 0;i<n;i++){
+            cout << arr[i] <<" ";
+        
+    }cout<<" "<<endl;
+
+    bubble_sort(arr,n);
         for(int i = 0;i<n;i++){
             cout << arr[i] <<" ";
         
