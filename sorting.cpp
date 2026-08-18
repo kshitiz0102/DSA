a2cb79ff9c97572b802d5b56a4d0abf37595adcc
Kshitiz01 <kshitizv289@gmail.com>
2026-08-18 15:00:41 +0530

    insertion sort

--- sorting.cpp
+++ sorting.cpp
@@ -27,6 +27,19 @@ void bubble_sort(int arr[],int n){
         }
     }
 }
+
+void insertion_sort(int arr[], int n ){
+    for(int i = 0;i<=n-1;i++){
+        int j = i;
+        while(j > 0 && arr[j-1] > arr[j]){
+            int temp = arr[j-1];
+            arr[j-1] = arr[j];
+            arr[j] = temp;
+
+            j--;
+        }
+    }
+}
 int main(){
 
     int n;
@@ -42,6 +55,13 @@ int main(){
     }cout<<" "<<endl;
 
     bubble_sort(arr,n);
+        for(int i = 0;i<n;i++){
+            cout << arr[i] <<" ";
+        
+    }cout<<" "<<endl;
+
+    
+    insertion_sort(arr,n);
         for(int i = 0;i<n;i++){
             cout << arr[i] <<" ";
         
